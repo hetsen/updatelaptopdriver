512ac023fe6e00af6770e88dacb80b48b497dd99
hetsen <creftan@gmail.com>
2021-11-25 09:38:23 +0100

    Update driverhelp.ps1

--- driverhelp.ps1
+++ driverhelp.ps1
@@ -1,18 +1,42 @@
-Out-File $env:USERPROFILE\desktop\driverhelp.txt
-$obj = Get-CimInstance win32_PnPSignedDriver | where deviceclass -like 'display' | where manufacturer -notlike '*INTEL*' | Select-Object -Property DeviceID
-$objTwo = "%NVIDIA_DEV." + ($obj."DeviceID").Substring(17,4) + "." + ($obj."DeviceID").Substring(29,4) + "." + ($obj."DeviceID").Substring(33,4) + "% = Section097, " + $obj."DeviceID".Substring(0, $obj."DeviceID".lastIndexOf('\'))
-$objThree = "NVIDIA_DEV." + ($obj."DeviceID").Substring(17,4) + "." + ($obj."DeviceID").Substring(29,4) + "." + ($obj."DeviceID").Substring(33,4) + ' = "NVIDIA Quadro M2000M"'
-Write-Host $objTwo
-Write-Host " "
-Write-Host $objThree
-$path = Get-Location
-"Replace 'Section97' with another section that is already in the nvdm.inf/nvdmwi.inf-file." | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-" " | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-" " | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-$objTwo | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-" " | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-" " | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-"Just add this at the end of the nvdm.inf/nvdmwi.inf-file by the other with the same format." | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-" " | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-" " | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
-$objThree | Out-File $env:USERPROFILE\desktop\driverhelp.txt -append
+$filePath = $(Get-Location).Path + "\Display.Driver\nvdmwi.inf";
+
+$devLine = (Select-String -Path $filePath -Pattern "%NVIDIA_DEV." | Select-Object -Last 1).Line.replace('I\V', 'I\\V')
+$devLineTwo = (Select-String -Path $filePath -Pattern "NVIDIA Quadro M2000M" | Select-Object -Last 1).Line
+
+Write-Host $devLine
+Write-Host $devLineTwo
+
+$device 	= Get-CimInstance win32_PnPSignedDriver | where deviceclass -like 'display' | where manufacturer -notlike '*INTEL*' | Select-Object -Property DeviceID
+$sectionLine	= "%NVIDIA_DEV." + ($device."DeviceID").Substring(17,4) + "." + ($device."DeviceID").Substring(29,4) + "." + ($device."DeviceID").Substring(33,4) + "% = Section097, " + $device."DeviceID".Substring(0, $device."DeviceID".lastIndexOf('\')) + "; You added this"
+$stringsLine 	= "NVIDIA_DEV." + ($device."DeviceID").Substring(17,4) + "." + ($device."DeviceID").Substring(29,4) + "." + ($device."DeviceID").Substring(33,4) + ' = "NVIDIA Quadro M2000M"; You added this'
+
+$Pattern = "$devLine"
+[System.Collections.ArrayList]$file = Get-Content $filePath
+$insert = @()
+
+for ($i=0; $i -lt $file.count; $i++) {
+  if ($file[$i] -match $pattern) {
+    $insert += $i+1
+  }
+}
+
+$insert | ForEach-Object { $file.insert($_,$sectionLine) }
+
+Set-Content $filePath $file
+
+$Pattern = "$devLineTwo"
+[System.Collections.ArrayList]$file = Get-Content $filePath
+$insert = @()
+
+for ($i=0; $i -lt $file.count; $i++) {
+  if ($file[$i] -match $pattern) {
+    $insert += $i+1
+  }
+}
+
+$insert | ForEach-Object { $file.insert($_,$devLineTwo) }
+
+Set-Content $filePath $file
+
+
+
